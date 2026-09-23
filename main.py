131dbc6f49f5f24e0f913ccdff288568ed3b5280
WhiteByte <animationr86@gmail.com>
2026-09-23 10:31:30 +0330

    Enable legacy Qt selection before GUI import

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import os
+import sys
+
+if "--legacy-windows" in sys.argv:
+    os.environ["MATH_EQUALIZER_LEGACY_WINDOWS"] = "1"
+
 from math_equalizer.app import run
+
 
 if __name__ == "__main__":
     run()
